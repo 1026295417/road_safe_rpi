e55c014b5347ff28d76766e1500be1f7b22aab06
ch-tseng <ch.tseng@sunplusit.com>
2019-09-11 23:10:29 +0800

    update

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##source /home/pi/envs/ROAD/bin/activate
+
+source /opt/intel/openvino/bin/setupvars.sh
+sh /opt/intel/openvino/install_dependencies/install_NCS_udev_rules.sh
+cd /home/pi/works/road_safe/rpi
+/home/pi/envs/ROAD/bin/python main.py
